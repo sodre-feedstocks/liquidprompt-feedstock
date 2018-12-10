ab038e301e7880d377d3fc1235fb94e1b64ef209
Patrick Sodré <psodre@gmail.com>
2018-12-09 22:12:52 -0800

    [win] Add windows to build scripts
      - Bumped build number
      - Moved sha256 into project body
      - grep is quiet

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set +x
+set -euf
 
 set windows=
 if [[ $OS == Windows* ]]; then
@@ -10,21 +10,25 @@ fi
 # there is no build step
 
 export prefix=$PREFIX
+export activate_prefix=$PREFIX
+export etc_lp='${CONDA_PREFIX}/etc/liquidprompt'
+
 if [ ! -z ${windows} ]; then
-   exit 1
-else
-   sed -e "s|/etc/liquidpromptrc|$PREFIX/etc/liquidpromptrc|g" \
-       -i \
-       liquidprompt
-   sed -e "s|~/.config/liquidprompt/nojhan.theme|$PREFIX/etc/liquidprompt/liquid.theme|" \
-       -e "s|~/.config/liquidprompt/nojhan.ps1|$PREFIX/etc/liquidprompt/liquid.ps1|" \
-       -i \
-       liquidpromptrc-dist
-
-   mkdir -p $PREFIX/etc/conda/activate.d
-   cp liquidprompt $PREFIX/etc/conda/activate.d/liquidprompt.sh
-   cp liquidpromptrc-dist $PREFIX/etc/liquidpromptrc
-
-   mkdir -p $PREFIX/etc/liquidprompt
-   cp liquid.ps1 liquid.theme $PREFIX/etc/liquidprompt
+  export prefix=$LIBRARY_PREFIX
+  export etc_lp='$(cygpath ${CONDA_PREFIX})/Library/etc/liquidprompt'
 fi
+
+sed -e "s|/etc/liquidpromptrc|${etc_lp}rc|g" \
+    -i \
+    liquidprompt
+sed -e "s|~/.config/liquidprompt/nojhan.theme|${etc_lp}/liquid.theme|" \
+    -e "s|~/.config/liquidprompt/nojhan.ps1|${etc_lp}/liquid.ps1|" \
+    -i \
+    liquidpromptrc-dist
+
+mkdir -p ${activate_prefix}/etc/conda/activate.d
+cp liquidprompt ${activate_prefix}/etc/conda/activate.d/liquidprompt.sh
+cp liquidpromptrc-dist ${prefix}/etc/liquidpromptrc
+
+mkdir -p ${prefix}/etc/liquidprompt
+cp liquid.ps1 liquid.theme ${prefix}/etc/liquidprompt
